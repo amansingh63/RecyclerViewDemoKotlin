6af7d7ff24d7f8686afad70fe779406b01726af7
Amanjot Singh <amanjot.singh@click-labs.com>
2017-08-14 17:31:29 +0530

    Addded some comments

--- app/src/main/java/com/amansingh/recyclerviewdemokotlin/MainActivity.kt
+++ app/src/main/java/com/amansingh/recyclerviewdemokotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.amansingh.recyclerviewdemokotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Add songs in list
         for (i in 0..10) {
             songList.add(SongDetails(i, "Song No. $i"))
         }
 
+        //synthetic loading of xml layout id of activity
         main_rv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         main_rv.adapter = RecyclerAdapter(songList)
 
     }
+
 }
