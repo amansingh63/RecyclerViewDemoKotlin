812d53bc9caead835d6f4c6e609389080666628d
Amanjot Singh <amanjot.singh@click-labs.com>
2017-08-14 17:29:45 +0530

    Added on click Listeners on itemView and textView

--- app/src/main/java/com/amansingh/recyclerviewdemokotlin/RecyclerAdapter.kt
+++ app/src/main/java/com/amansingh/recyclerviewdemokotlin/RecyclerAdapter.kt
@@ -1,9 +1,11 @@
 package com.amansingh.recyclerviewdemokotlin
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.item_view_rv.view.*
 
 /**
@@ -15,6 +17,10 @@ class RecyclerAdapter(val songList: ArrayList<SongDetails>) : RecyclerView.Adapt
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.bindItems(songList[position])
+        holder.itemView.setOnClickListener { Toast.makeText(holder.itemView.context, "itemView clicked $position", Toast.LENGTH_SHORT).show() }
+        holder.itemView.tv_song_title.setOnClickListener {
+            holder.itemView.tv_song_title.context.startActivity(Intent(holder.itemView.context, TestActivity::class.java))
+        }
 
     }
 
@@ -24,6 +30,7 @@ class RecyclerAdapter(val songList: ArrayList<SongDetails>) : RecyclerView.Adapt
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_view_rv, parent, false))
+
     }
 
     // My view holder Class in whit constructor parameter
@@ -31,7 +38,7 @@ class RecyclerAdapter(val songList: ArrayList<SongDetails>) : RecyclerView.Adapt
 
         fun bindItems(songDetail: SongDetails) {
             itemView.tv_song_id.text = songDetail.id.toString()
-            itemView.tv_song_title.text = songDetail.title.toString()
+            itemView.tv_song_title.text = songDetail.title
         }
 
     }
